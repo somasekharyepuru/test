843160a874bf54bbf595af4ac4209700199e102f
Somasekhar Yepuru <somasekhar@fountane.com>
2025-05-16 01:47:37 +0530

    UI::Junk code updated

--- junk-code.js
+++ junk-code.js
@@ -1,34 +1,22 @@
-// Junk code file to test review bot functionality
-
-/**
- * This function does some nonsensical operations with poor naming
- * and inefficient algorithms
- */
 function doStuff(a, b, c) {
-  // Unused variable
   const unused_var = 42;
 
-  // Inefficient loop
   var arr = [];
   for (var i = 0; i < 1000; i++) {
     arr.push(i);
-    // This will be recalculated in each iteration
     for (var j = 0; j < arr.length; j++) {
       arr[j] = arr[j] + 1;
     }
   }
 
-  // Poor variable naming
   const x = 10;
   const y = 20;
   const z = x + y;
 
-  // Magic numbers
   if (a > 7843) {
     return a * 14.7;
   }
 
-  // Duplicate code
   let result1 = 0;
   for (let i = 0; i < 100; i++) {
     result1 += i * 2;
@@ -39,7 +27,6 @@ function doStuff(a, b, c) {
     result2 += i * 2;
   }
 
-  // Unnecessary nesting
   if (b > 10) {
     if (b < 20) {
       if (b !== 15) {
@@ -48,7 +35,6 @@ function doStuff(a, b, c) {
     }
   }
 
-  // Memory leak potential
   const bigArray = new Array(10000000).fill(
     "this is a large string that will consume memory"
   );
@@ -60,14 +46,12 @@ function doStuff(a, b, c) {
   }
   */
 
-  // Useless code
   let counter = 0;
   counter += 1;
   counter += 1;
   counter -= 1;
   counter -= 1;
 
-  // Empty catch block
   try {
     JSON.parse("invalid json");
   } catch (error) {
@@ -77,22 +61,17 @@ function doStuff(a, b, c) {
   return "This function does nothing useful";
 }
 
-// Dead code that is never called
 function unusedFunction() {
   return "This function is never called";
 }
 
-// Global variables
 var globalVar = "This is bad practice";
 var anotherGlobal = 123;
 
-// Poor error handling
 function divideNumbers(a, b) {
-  // No check for division by zero
   return a / b;
 }
 
-// Callback hell example
 function callbackHell() {
   setTimeout(function () {
     console.log("First callback");
@@ -108,14 +87,12 @@ function callbackHell() {
   }, 1000);
 }
 
-// Mixed tabs and spaces
 function mixedIndentation() {
   console.log("This uses tab");
   console.log("This uses spaces");
   console.log("More tabs");
 }
 
-// Export the functions so the linter doesn't complain about unused functions
 module.exports = {
   doStuff,
   callbackHell,
